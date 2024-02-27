f1413352492e1a5fdfcab05ba9631816187252fa
David <david.d.hernandez@accenture.com>
2024-02-27 12:52:52 -0600

    Cleaning Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,22 @@
 import { Grid, Typography } from "@mui/material";
+import { Container } from "./Header.styles"
 
 function Header() {
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      padding={2}
-    >
+    <Container container>
       <Grid item flexGrow={1}>
-        <Typography
-          variant="caption"
-          color="primary"
-          component="div"
-          alignItems="center"
-        >
+        <Typography variant="caption" color="primary" alignItems="center">
           <span style={{ fontSize: "1.5rem" }}>[ </span>
           Making your Life Easier
           <span style={{ fontSize: "1.5rem" }}> ]</span>
         </Typography>
       </Grid>
       <Grid item flexGrow={1}>
-        <Typography variant="h4">
-          <strong>Discovering the World</strong>
+        <Typography variant="h4" fontWeight="bold">
+          Discovering the World
         </Typography>
       </Grid>
-    </Grid>
+    </Container>
   );
 }
 
